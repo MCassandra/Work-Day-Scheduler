85e643e469605f99fe7caa0abeac644a608b8949
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-07-20 07:25:03 -0700

    trying to to display saved local storage value on page

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,10 @@ var saveBtn = document.querySelector(".saveBtn");
 var inputTask = document.querySelector(".task");
 
 
-
+function showSavedTask(){
+    var saved = localStorage.getItem("task");
+    text1.textContent = saved;
+}
 function saveInput (){
     var needToDo = text1.value;
     console.log (needToDo);
@@ -11,6 +14,8 @@ function saveInput (){
     if (needToDo){
         localStorage.setItem("task", text1.value);
     }
+    showSavedTask();
 }
+
 // when click save button, save key-value pairs into local storage
 saveBtn.addEventListener("click", saveInput);
