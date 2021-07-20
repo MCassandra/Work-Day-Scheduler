7d90ccfdac00d5eb5aa021031f74c4298985f2fb
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-07-19 17:17:24 -0700

    update tables and redo update local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,19 @@
 var saveButton=document.querySelector(".saveBtn");
-
-// need a function to get input to local storage
-function save(){
-    localStorage.getItem("inputTask", savedTask);
-}
+var updatetext = document.querySelector("#text");
+var storageInput = document.querySelector(".storage");
 
 // on click, set input to local storage
 saveButton.addEventListener("click", function(event){
     event.preventDefault();
-    var savedTask=document.getElementById("task").value;
-    localStorage.setItem("inputTask", savedTask);
-    save();
+    
 })
 
+var savedTask=document.getElementById(".task").value;
+    document.localStorage.setItem("inputTask", savedTask);
 
-
+    // need a function to get input to local storage
+function save(){
+    document.localStorage.getItem("inputTask", savedTask);
+}
+updatetext.document.textContent.savedTask
+save();
