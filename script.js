2595843c4892aceb2c25e545d37d00ade11f9960
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-10-05 11:00:20 -0700

    started from scratch

--- script.js
+++ script.js
@@ -1,40 +1,87 @@
 var displayDate = document.getElementById("now");
-var text1 = document.getElementById("text1");
-var saveBtn = document.querySelectorAll(".saveBtn");
-// var inputTask = document.querySelector(".task");
-var textareaEl = document.querySelectorAll(".task")
+var saveButton = document.querySelectorAll(".saveBtn");
+var textEls = document.querySelectorAll("textarea");
+
+
+
+saveButton.forEach(button => {
+    button.addEventListener("click", function (){
+        var textEl = this.parentNode.childNodes[3];
+        console.log(textEl.value);
+        console.log(textEl.id);
+        console.log(todaysTasks[textEl.id]);
+        todaysTasks[textEl.id].task = textEl.value;
+        saveTask();
+    })
+})
 
 // show today's date
-function todaysDate (){
+function todaysDate() {
     var today = moment().format("MMMM DD, YYYY");
-    displayDate.textContent=today;
+    displayDate.textContent = today;
 }
 
-// get the stored value from local storage and show it on page, even when refreshed
-function showSavedTask(){
-    var saved = localStorage.getItem("task");
-    text1.textContent = saved;
-    
+// create a variable to loop through the times
+var todaysTasks = JSON.parse(localStorage.getItem("todaysTask")) || {
+    "9":{
+        hour: "9",
+        task: "",
+    },
+    "10":{
+        hour: "10",
+        task: "",
+    },
+    "11":{
+        hour: "11",
+        task: "",
+    },
+    "12":{
+        hour: "12",
+        task: "",
+    }, 
+    "13":{
+        hour: "13",
+        task: "",
+    }, 
+    "14":{
+        hour: "14",
+        task: "",
+    }, 
+    "15":{
+        hour: "15",
+        task: "",
+    }, 
+    "16":{
+        hour: "16",
+        task: "",
+    },
 }
 
-// get user's input and store the value
-function saveInput (e){
-    // got code structure (line 22) from a BCS assistant  
-    textareaEl=(e.target.getAttribute("hour"));
-    
-    var needToDo = text1.value;
-    console.log (textareaEl);
-// make sure input is entered / no empty submitts
-    if (needToDo){
-        localStorage.setItem("task", text1.value);
-    }
-    showSavedTask();
-}
-showSavedTask();
+textEls.forEach(textEl => {
+    textEl.value = todaysTasks[textEl.id].task
+});
+
+// save tasks to local storage
+function saveTask() {
+    localStorage.setItem("todaysTask", JSON.stringify(todaysTasks));
+    console.log("saved");
+};
+
+
+// // display any tasks in local storage on page
+// function displayTask() {
+//     todaysTasks.forEach(task =>{
+//         // console.log(task);
+//     }
+//     )
+// };
+
+// displayTask();
+// shows existing data in local storage if it exists
+
+
 todaysDate();
 
-
-// when click save button, save key-value pairs into local storage
-for (var i = 0; i < saveBtn.length; i++){
-saveBtn[i].addEventListener("click", saveInput);
-};
+// saves tasks/data to be stored in local storage
+// var 
+// saveButton.addEventListener("click", saveTask);
